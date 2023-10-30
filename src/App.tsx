90c19fb810dd9148be434b9a37c1c6eee432c7b4
wold21 <wold2180@gmail.com>
2023-10-30 23:02:16 +0900

    -added login form -added router -added firebase init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,75 @@
-function App() {
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import CreateAccount from "./routes/create-account";
+import Login from "./routes/login";
+import styled, { createGlobalStyle } from "styled-components";
+import reest from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
-  return (
-    <>
-    </>
-  )
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "profile",
+                element: <Profile />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/create-account",
+        element: <CreateAccount />,
+    },
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reest};
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+  sans-serif;
+}
+`;
+
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
+function App() {
+    const [isLoading, setIsloading] = useState(true);
+    const init = async () => {
+        await auth.authStateReady();
+        await setIsloading(false);
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+    return (
+        <Wrapper>
+            <GlobalStyles />
+            {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+        </Wrapper>
+    );
 }
 
-export default App
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmLa17uYcdVemslMdAoS7xcIYvqg5Yl9M",
+  authDomain: "twitter-clone-for-react.firebaseapp.com",
+  projectId: "twitter-clone-for-react",
+  storageBucket: "twitter-clone-for-react.appspot.com",
+  messagingSenderId: "350128919759",
+  appId: "1:350128919759:web:9d8228fec5f621b2b853ba"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
